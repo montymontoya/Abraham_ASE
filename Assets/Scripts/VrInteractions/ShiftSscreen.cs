d2e170d3fa776b7467d53b5309a119ac8d9c1730
Fernando Montoya <montoya.manzano.fernando@gmail.com>
2019-04-29 20:47:06 -0500

    pantalla 50%

---
+++ Assets/Scripts/VrInteractions/ShiftSscreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+public class ShiftSscreen : Interactable
+{
+    public EVRButtonId pickupButton = EVRButtonId.k_EButton_SteamVR_Trigger;//Botón predefinido para sostener algo es el gatillo
+
+    public GameObject nxtScreen;
+    public GameObject thisScreen;
+
+    /************CON ESTO SE LLAMAN LOS METODOS DE LA INTERACCION DEPENDIENDO DEL BOTON PRESIONADO*************/
+    public override void ButtonPressDown(EVRButtonId button, ControllerInput controller)
+    {
+        thisScreen.SetActive(false);
+        nxtScreen.SetActive(true);
+    }
+
+    public override void ButtonPressUp(EVRButtonId button, ControllerInput controller)
+    {
+
+    }
+
+    /***********ESTO SE CARGA AL INICIO O PRIMER FRAME*********/
+    void Awake()
+    {
+
+    }
+    /*************/
+}
