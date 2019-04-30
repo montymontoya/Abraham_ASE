5b458b9b1784bb22a6c8a4040fe4f0d8fd2ecf9e
Fernando Montoya <montoya.manzano.fernando@gmail.com>
2019-04-30 15:17:24 -0500

    casiListo

---
+++ Assets/Scripts/MoveCursorLens.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class MoveCursorLens : Interactable
+{
+    public Transform lupa;
+    public float deg;
+    //public float frstDeg;
+    // Start is called before the first frame update
+    public EVRButtonId pickupButton = EVRButtonId.k_EButton_SteamVR_Trigger;//Botón predefinido para sostener algo es el gatillo
+
+
+    public override void ButtonPressDown(EVRButtonId button, ControllerInput controller)
+    {
+        if (button == pickupButton) // Si se presionó el botón de sostener
+        {
+
+            lupa.localEulerAngles=new Vector3(0,0,deg);
+        }
+    }
+}
---
+++ Assets/Scripts/VrInteractions/ShiftSscreenCircularMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+public class ShiftSscreenCircularMenu : Interactable
+{
+    public GameObject[] scrns2Hide;
+
+    public EVRButtonId pickupButton = EVRButtonId.k_EButton_SteamVR_Trigger;//Botón predefinido para sostener algo es el gatillo
+
+    public GameObject nxtScreen;
+
+    /************CON ESTO SE LLAMAN LOS METODOS DE LA INTERACCION DEPENDIENDO DEL BOTON PRESIONADO*************/
+    public override void ButtonPressDown(EVRButtonId button, ControllerInput controller)
+    {
+        if (button == pickupButton) // Si se presionó el botón de sostener
+        {
+            foreach (var item in scrns2Hide)
+            {
+                item.SetActive(false);
+            }
+            
+            nxtScreen.SetActive(true);
+        }
+    }
+}
