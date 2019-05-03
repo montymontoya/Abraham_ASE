7c5463276c31f5ed7d221a66c814909d764cb262
montymontoya <montoya.manzano.fernando@gmail.com>
2019-05-03 18:20:48 -0500

    otrosCambios

---
+++ Assets/Scripts/ZoneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneManager : MonoBehaviour
+{
+    public GameObject zone2Active,thisGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisGameObject = this.GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (thisGameObject.activeSelf)
+        {
+            zone2Active.SetActive(true);
+        }
+        
+    }
+}
